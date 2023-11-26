605f2770d2397f0ce781e1f3e05400db61549fc4
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-11-26 10:37:50 +0100

    Caching with TTL

--- src/app/caching.service.ts
+++ src/app/caching.service.ts
@@ -1,17 +1,41 @@
 import { HttpEvent } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+interface CacheEntry {
+  value: HttpEvent<unknown>;
+  expiresOn: number;
+}
+
+const TTL = 3_000;
+
 @Injectable({ providedIn: "root" })
 export class CachingService {
-  readonly #cache = new Map<string, HttpEvent<unknown>>();
+  readonly #cache = new Map<string, CacheEntry>();
 
   get(key: string): HttpEvent<unknown> | undefined {
-    return this.#cache.get(key);
+    const cached = this.#cache.get(key);
+
+    if (!cached) {
+      return undefined;
+    }
+
+    // 👇 Remove the entry if expired
+    const hasExpired = new Date().getTime() >= cached.expiresOn;
+    if (hasExpired) {
+      this.#cache.delete(key);
+      return undefined;
+    }
+
+    return cached.value;
   }
 
   set(key: string, value: HttpEvent<unknown>): void {
     if (key.includes("album")) {
-      this.set(key, value);
+      this.#cache.set(key, {
+        value,
+        // 👇 Set its lifespan
+        expiresOn: new Date().getTime() + TTL,
+      });
     }
   }
 }
